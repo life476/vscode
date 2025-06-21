441b4753a956b22f4a14fa697f6cbbe850cae20c
life476 <695034644@qq.com>
2025-06-21 12:20:25 +0800

    更新 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 import * as path from 'path';
 import * as fs from 'original-fs';
 import * as os from 'os';
